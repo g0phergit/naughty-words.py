a5107c53feb31949def4efea9c8282e6aee0396f
Gopher <gopherubuntouch@gmail.com>
2021-07-01 16:24:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="naughty_words_py",
-    version="0.0.1",
+    version="1.0.0",
     author="Gopher",
     author_email="gopherubuntouch@gmail.com",
     description="Naugty words parser for python",
