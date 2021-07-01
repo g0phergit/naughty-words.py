a88cd81fc2ea67673ea179130d73ca67bcb7f370
Gopher <gopherubuntouch@gmail.com>
2021-07-01 16:21:46 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="naughty_words_py",
+    version="0.0.1",
+    author="Gopher",
+    author_email="gopherubuntouch@gmail.com",
+    description="Naugty words parser for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/g0phergit/naughty-words.py",
+    project_urls={
+        "Bug Tracker": "https://github.com/g0phergit/naughty_words.py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
